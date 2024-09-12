c3c85dab29a2506816a83a701b328347fa81c62b
Vadim Korolev <vkorol98@gmail.com>
2024-09-12 14:28:09 +0300

    fix

--- m_video_ui/pages/cart_page.py
+++ m_video_ui/pages/cart_page.py
@@ -5,15 +5,11 @@ from selene import browser, have
 class CartPage:
     def add_product_to_cart(self):
         with allure.step('Добавить товар в корзину.'):
-            browser.all('[type="button"]').element_by(have.text('Добавить в корзину')).click()
+            browser.element('.cart-button').click()
 
     def move_to_cart(self):
         with allure.step('Перейти в корзину.'):
-            browser.all('[type="button"]').element_by(have.text('Перейти в корзину')).click()
-
-    def continue_buying(self):
-        with allure.step('Продолжить покупки.'):
-            browser.all('[type="button"]').element_by(have.text('Continue Shopping')).click()
+            browser.element('.cart-button').click()
 
     def remove_product_from_cart(self):
         with allure.step('Удалить продукт из корзины.'):
@@ -23,7 +19,7 @@ class CartPage:
         with allure.step('Очистить корзину.'):
             browser.element('.cart-list__delete-selected-button').click()
 
-    def check_product_in_cart(self, game_name):
+    def check_product_in_cart(self, product_name):
         with allure.step('Проверить продукт в корзине.'):
             browser.element('.cart-item__name').should(have.exact_text(product_name))
 
